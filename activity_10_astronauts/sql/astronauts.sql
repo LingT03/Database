4b5338f7006b91eef07791e666be9583935115e2
Ling Thang <Lingthang03@gmail.com>
2023-03-02 11:16:26 -0700

    act10

--- activity_10_astronauts/sql/astronauts.sql
+++ activity_10_astronauts/sql/astronauts.sql
@@ -8,16 +8,52 @@ CREATE DATABASE astronauts;
 /c astronauts; 
 
 CREATE TABLE astronauts (
-    id INT PRIMARY KEY,
-    lastname VARCHAR(50),
-    firstname VARCHAR(50),
-    suffix VARCHAR(50),
-    gender VARCHAR(50),
-    birth Date,
-    city VARCHAR(50),
-    state VARCHAR(50),
-    country VARCHAR(50),
+    id SERIAL PRIMARY KEY,
+    lastname VARCHAR(50) NOT NULL,
+    firstname VARCHAR(50) NOT NULL,
+    suffix VARCHAR(5),
+    gender VARCHAR(50) NOT NULL,
+    birth Date NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    state VARCHAR(50) NOT NULL,
+    country VARCHAR(50) NOT NULL,
     status VARCHAR(50),
-    daysInSpace INT,
-    Flights INT,
+    daysInSpace INT NOT NULL,
+    Flights INT NOT NULL,
 );
+
+copy Astronauts 
+  (lastName,firstName,suffix,gender,birth,city,state,country,status,daysInSpace,Flights)
+    from /Users/lingthang/CSdatabase/activity_10_astronauts/sql/astronauts.csv with DELIMITER ',' CSV HEADER;
+
+/*
+a) the total number of astronauts. 
+
+b) the total number of American astronauts. 
+
+c) the list of nationalities of all astronauts in alphabetical order. 
+
+d) all astronaut names ordered by last name (use the format Last Name, First Name, Suffix to display the names). 
+
+e) the total number of astronauts by gender. 
+
+f) the total number of female astronauts that are still active. 
+
+g) the total number of American female astronauts that are still active. 
+
+h) the list of all American female astronauts that are still active ordered by last name (use the same name format used in d). 
+
+i) the list of Chinese astronauts, displaying only their names and ages (use the same name format used in d). 
+
+j) the total number of astronauts by country. 
+
+k) the total number of American astronauts per state ordered by the totals in
+descending order. 
+
+l) the total number of astronauts by statuses (i.e., active or retired). 
+
+m) name and age of all non-American astronauts in alphabetical order (use the same name format used in d). 
+
+n) the average age of all American astronauts that are still active. 
+
+*/
