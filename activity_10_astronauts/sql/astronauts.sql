45d817d305c5f5ba192d3de2a02571424adaabd9
Ling Thang <Lingthang03@gmail.com>
2023-03-02 11:02:38 -0700

    act10

--- activity_10_astronauts/sql/astronauts.sql
+++ activity_10_astronauts/sql/astronauts.sql
@@ -1,4 +1,23 @@
 -- CS3810: Principles of Database Systems
 -- Instructor: Thyago Mota
+-- Student: Ling Thang
 -- Description: The astronauts database
 
+CREATE DATABASE astronauts;
+
+/c astronauts; 
+
+CREATE TABLE astronauts (
+    id INT PRIMARY KEY,
+    lastname VARCHAR(50),
+    firstname VARCHAR(50),
+    suffix VARCHAR(50),
+    gender VARCHAR(50),
+    birth Date,
+    city VARCHAR(50),
+    state VARCHAR(50),
+    country VARCHAR(50),
+    status VARCHAR(50),
+    daysInSpace INT,
+    Flights INT,
+);
