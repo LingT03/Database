6cc40838b672b77430174e6eca5fea84c10c68da
Ling Thang <Lingthang03@gmail.com>
2023-03-10 21:29:31 -0700

    HWK5

--- hwk_05_occupations/src/occupations.sql
+++ hwk_05_occupations/src/occupations.sql
@@ -1,6 +1,6 @@
 -- CS3810: Principles of Database Systems
 -- Instructor: Thyago Mota
--- Student: 
+-- Student: Ling Thang 
 -- Description: a database of occupations
 
 CREATE DATABASE occupations;
@@ -11,9 +11,9 @@ DROP TABLE IF EXISTS Occupations;
 
 -- TODO: create table Occupations
     CREATE TABLE Occupations (
-        code VARCHAR(10),
-        occupation VARCHAR(100),
-        "job_family" VARCHAR(100)
+        code VARCHAR(20),
+        occupation VARCHAR(300),
+        "job_family" VARCHAR(300)
     );
 -- TODO: populate table Occupations
 \COPY Occupations(Code, Occupation, "job_family") FROM '/var/lib/postgresql/data/occupations.csv' DELIMITER ',' QUOTE '"' CSV HEADER;
@@ -23,8 +23,6 @@ DROP TABLE IF EXISTS Occupations;
 
 -- TODO: b) a list of all job families in alphabetical order (expect 23).
     SELECT DISTINCT "job_family" FROM occupations ORDER BY 1;
-    -- or 
-    SELECT "job_family" FROM occupations ORDER BY "job_family";
 
 -- TODO: c) the total number of job families (expect 23)
     SELECT COUNT(*) FROM (
@@ -32,14 +30,16 @@ DROP TABLE IF EXISTS Occupations;
     ) subquery;
 
 -- TODO: d) the total number of occupations per job family in alphabetical order of job family.
-    SELECT "job_family", COUNT(*) AS "total_occupations" FROM occupations GROUP BY "job_family" ORDER BY "job_family";
+    SELECT "job_family", COUNT(*) AS "total_occupations" FROM occupations GROUP BY "job_family" ORDER BY 1;
 
 -- TODO: e) the number of occupations in the "Computer and Mathematical" job family (expect 38)
     SELECT COUNT(*) AS "number_of_occupations" FROM occupations WHERE "job_family" = 'Computer and Mathematical';
 
 -- BONUS POINTS
 -- TODO: f) an alphabetical list of occupations in the "Computer and Mathematical" job family.
-    SELECT "occupation" FROM occupations WHERE "job_family" = 'Computer and Mathematical' ORDER BY "occupation" ASC;
+    SELECT "occupation" FROM occupations WHERE "job_family" = 'Computer and Mathematical' ORDER BY 1;
+    -- NOTE: ORDER BY 1 is the same as ORDER BY "occupation" and ORDER BY defualts to alphabetical order
 
 -- TODO: g) an alphabetical list of occupations in the "Computer and Mathematical" job family that begins with the word "Database"
     SELECT "occupation" FROM occupations WHERE "job_family" = 'Computer and Mathematical' AND "occupation" LIKE 'Database%' ORDER BY "occupation" ASC;
+    -- ASC is the default order, but I included it for clarity
