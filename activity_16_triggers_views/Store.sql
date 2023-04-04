a67e7bc1f9e9534fbc8072f66374b79764009907
Ling Thang <Lingthang03@gmail.com>
2023-04-04 10:17:25 -0600

    commit

---
+++ activity_16_triggers_views/Store.sql
@@ -0,0 +1,15 @@
+Create DATABASE Store;
+
+CREATE TABLE Orders (
+    number serial PRIMARY KEY,
+    date date NOT NULL
+);
+
+CREATE TABLE Items (
+
+);
+
+CREATE TABLE Products (
+    id serial PRIMARY KEY,
+    
+);
