fe05c0511c14e1035168183ff3164e49c4a36814
Ling Thang <Lingthang03@gmail.com>
2023-04-11 12:51:11 -0600

    act20

--- activity_20_sqlite/dbsetup.py
+++ activity_20_sqlite/dbsetup.py
@@ -1,25 +1,21 @@
 import configparser as cp
 from sqlalchemy import create_engine
-from sqlalchemy.engine import create_engine
-from sqlalchemy.engine import URL 
+from sqlalchemy.engine import URL
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
-
 Base = declarative_base()
-
-class Pl(Base):
-    __tablename__ = 'pls'
+class PL(Base): 
+    __tablename__ = "pls"
     id = Column(Integer, primary_key=True)
     name = Column(String)
-
+    
 engine = create_engine('sqlite:///pls.db')
-if engine:
-    print('Connection to SQLite database was successful!')
+if engine: 
+    print('Connection to Postgres database was successful!')
     Session = sessionmaker(engine)
-    session= Session()
-    query = session.query(Pl)
-    for pl in query.all:
+    session = Session()
+    query = session.query(PL)
+    for pl in query.all():
         print(pl.id, pl.name)
-    print('Bye!')
