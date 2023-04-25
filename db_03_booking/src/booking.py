9d2c1601d077021db51970a823e04e3571959f74
Ling Thang <Lingthang03@gmail.com>
2023-04-25 10:02:05 -0600

    db_03

--- db_03_booking/src/booking.py
+++ db_03_booking/src/booking.py
@@ -1,7 +1,7 @@
 """
 CS3810: Principles of Database Systems
 Instructor: Thyago Mota
-Student(s): 
+Student(s): Ling Thang 
 Description: A room reservation system
 """
 
@@ -51,25 +51,52 @@ def db_connect():
 
 # TODO: display all reservations in the system using the information from ReservationsView
 def list_op(conn):
-    pass
+     with conn.cursor() as cur:
+        cur.execute('SELECT * FROM ReservationsView;')
+        rows = cur.fetchall()
+        print('Reservations:')
+        for row in rows:
+            print(row)
+
 
 # TODO: reserve a room on a specific date and period, also saving the user who's the reservation is for
 def reserve_op(conn):
-    pass
+    abbr = input('Abbreviation: ')
+    room = input('Room: ')
+    date = input('Date (YYYY-MM-DD): ')
+    period = input('Period (1-5): ')
+    user = input('User: ')
+    with conn.cursor() as cur:
+        cur.execute('EXECUTE QueryReservationExists (%s, %s, %s, %s);', (abbr, room, date, period))
+        if cur.fetchone() is None:
+            cur.execute('EXECUTE NewReservation (%s, %s, %s, %s);', (abbr, room, date, period))
+        cur.execute('EXECUTE UpdateReservationUser (%s, %s, %s, %s, %s);', (user, abbr, room, date, period))
+        conn.commit()
+        print('Reservation created.')
 
 # TODO: delete a reservation given its code
 def delete_op(conn):
-    pass
+    code = input('Code: ')
+    with conn.cursor() as cur:
+        cur.execute('EXECUTE QueryReservationExistsByCode (%s);', (code,))
+        if cur.fetchone() is None:
+            print('Reservation not found.')
+        else:
+            cur.execute('EXECUTE DeleteReservation (%s);', (code,))
+            conn.commit()
+            print('Reservation deleted.')
 
 if __name__ == "__main__":
-    with db_connect() as conn: 
+    with db_connect() as conn:
         op = 0
         while op != 4: 
             menu()
-            op = int(input('? '))
+            op = int(input('\nEnter your option: '))
             if op == 1: 
                 list_op(conn)
             elif op == 2:
                 reserve_op(conn)
             elif op == 3:
                 delete_op(conn)
+            elif op == 4:
+                print ('Goodbye!')
