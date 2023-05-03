fd406cbaf7ce1dd3b2bc5cfc8d1dd7f9dda1c747
Ling Thang <Lingthang03@gmail.com>
2023-05-03 15:12:32 -0600

    mongo

--- hwk_10_books/books.js
+++ hwk_10_books/books.js
@@ -1,64 +1,136 @@
 // CS 3810: Principles of Database Systems
 // Homework 10: The books database
 // Instructor: Thyago Mota
-// Student: 
+// Student:
 
 // db creation
-use books;
+//use books;
 
 db.books.insertOne({
-  isbn: '1933988673',
-  title: 'Unlocking Android',
-  author: 'Charlie Collins',
+  isbn: "1933988673",
+  title: "Unlocking Android",
+  author: "Charlie Collins",
   date: { year: 2009, month: 4 },
-  pages: 250, 
-  genres: ['technical']
+  pages: 250,
+  genres: ["technical"],
 });
 
 db.books.insertOne({
-  isbn: '1935182722',
-  title: 'Android in Action, Second Edition',
-  author: 'Robi Sen',
+  isbn: "1935182722",
+  title: "Android in Action, Second Edition",
+  author: "Robi Sen",
   date: { year: 2011, month: 1 },
-  pages: 120, 
-  genres: ['technical', 'programming']
+  pages: 120,
+  genres: ["technical", "programming"],
 });
 
 db.books.insertOne({
-  isbn: '1617290084',
-  title: 'Specification by Example',
-  author: 'Gojko Adzic',
+  isbn: "1617290084",
+  title: "Specification by Example",
+  author: "Gojko Adzic",
   date: { year: 2011, month: 6 },
-  pages: 840, 
-  genres: ['technical', 'software engineering']
+  pages: 840,
+  genres: ["technical", "software engineering"],
 });
 
 db.books.insertOne({
-  isbn: '1933988797',
-  title: 'Flex on Java',
-  author: 'Andres Almiray',
+  isbn: "1933988797",
+  title: "Flex on Java",
+  author: "Andres Almiray",
   date: { year: 2010, month: 10 },
-  pages: 95, 
-  genres: ['technical', 'programming']
+  pages: 95,
+  genres: ["technical", "programming"],
 });
 
 db.books.insertOne({
-  isbn: '1617291609',
-  title: 'MongoDB in Action',
-  author: 'Kyle Banker',
+  isbn: "1617291609",
+  title: "MongoDB in Action",
+  author: "Kyle Banker",
   date: { year: 2012, month: 5 },
-  pages: 350, 
-  genres: ['technical', 'databases']
+  pages: 350,
+  genres: ["technical", "databases"],
 });
 
 // list all books titles only (excluding _id)
+db.books.aggregate([
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+]);
 
 // list all books written by 'Kyle Banker
+db.books.aggregate([
+  {
+    $match: {
+      author: "Kyle Banker",
+    },
+  },
+
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+]);
 
 // list all books published in 2011
+db.books.aggregate([
+  {
+    $match: {
+      "date.year": 2011,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+]);
 
 // list the number of books per year sorted by year
+db.books.aggregate([
+  {
+    $group: {
+      _id: "$date.year",
+      count: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      _id: 1,
+    },
+  },
+]);
 
 // list all books that have more than 100 pages but less than 500 pages
+db.books.aggregate([
+  {
+    $match: {
+      pages: {
+        $gt: 100,
+        $lt: 500,
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+]);
 
-// bonus: list the average number of pages of books per gender 
+// bonus: list the average number of pages of books per gender
+db.books.aggregate([
+  {
+    $group: {
+      _id: "$gender",
+      avgPages: { $avg: "$pages" },
+    },
+  },
+]);
